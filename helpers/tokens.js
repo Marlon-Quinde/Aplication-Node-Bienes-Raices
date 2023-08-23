838497e551275a75738349605bf030f8cb37e094
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-08-22 23:19:15 -0500

    actualizacion

---
+++ helpers/tokens.js
@@ -0,0 +1,4 @@
+const generarId = () =>
+  Math.random().toString(32).substring(2) + Date.now().toString(32);
+
+export { generarId };
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+// const express = require("express"); // Common JS
+import express from "express"; //ES6
+import usuarioRoutes from "./auth/routes";
+import db from "../config/db";
+
+// Crear la App
+const app = express();
+
+//Habilitar lectura datos de formulario
+app.use(express.urlencoded({ extended: true }));
+
+//Conexion a db
+async function main() {
+  try {
+    await db.authenticate();
+    await db.sync();
+    console.log("conexion correcta");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+
+// Habilitar pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+//Carpeta Publica
+app.use(express.static("public"));
+
+// Routing
+app.use("/auth", usuarioRoutes);
+
+//?Definir un puerto y arrancar el proyecto
+const port = 3000;
+app.listen(port, () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
