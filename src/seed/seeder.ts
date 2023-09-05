972cd3d1e32145e673f940e6f2f3c00f6670d5ec
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-09-05 00:40:21 -0500

    feat(precios): :sparkles: Añadido seeder de precios

--- src/seed/seeder.ts
+++ src/seed/seeder.ts
@@ -1,5 +1,7 @@
 import categorias from "./categoria";
 import Categoria from "../models/Categoria";
+import precios from "./precios";
+import Precio from "../models/Precio";
 import db from "../config/db";
 const importarDatos = async () => {
   try {
@@ -11,7 +13,10 @@ const importarDatos = async () => {
     await db.sync();
 
     //Insertar los datos
-    await Categoria.bulkCreate(categorias);
+    await Promise.all([
+      Categoria.bulkCreate(categorias),
+      Precio.bulkCreate(precios),
+    ]);
     console.log("Datos importados correctamente");
     process.exit(0);
   } catch (error) {
